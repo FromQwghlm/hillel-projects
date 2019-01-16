389878a7150d535c3a8ba77f2771e6c11d6181a5
Dmi3 <dmi3.kuznesov@gmail.com>
2019-01-16 17:29:03 +0200

    some corrections

--- 04.loops/iterations.js
+++ 04.loops/iterations.js
@@ -14,14 +14,19 @@ console.log('DO WHILE');
 var i = 1;
 do {
     console.log(i);
-    i++
+    i++;
 } while (!(i % 2));
+
+
 // //////////////////////////////
 console.log('WHILE');
 
 var i = 1;
 while (!(i % 2)) {
-    i++ 
-    console.log(i); 
+    console.log(i);
+    i++;
 } 
+console.log(i);
+
+
 
--- 04.loops/multipleLowIteration.js
+++ 04.loops/multipleLowIteration.js
@@ -5,20 +5,9 @@ var multiple = +prompt('Введите число к которому будем
 var maxNum = +prompt('Сколько кратных чисел вывести?', 10);
 var multipleList = [];
 
-// FOR SECTION
-for (var i = 1; multipleList.length < maxNum; i++) {
-	if (i % multiple === 0) multipleList.push(i);       
+console.log('// Числа кратные ' + multiple + ':');
+for (var i = 1; i <= maxNum; i++) {
+	multipleList.push(i * multiple);      
 }
 console.log(...multipleList);
 
-// WHILE SECTION
-var multipleList = [];
-var i = 1;
-while (multipleList.length !== maxNum) {
-	if (i % multiple === 0) {
-        console.log(`add ${i} to multipleList`);
-        multipleList.push(i);
-    }
-	i++;
-}
-console.log(...multipleList);
